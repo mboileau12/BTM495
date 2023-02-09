b3275afd206d34dd48f5afb0b91ea3f6afef7a44
mboileau12 <matboileau@live.ca>
2023-02-09 16:41:20 -0500

    Amenity Class

---
+++ HotelMortagneReservationSystem/Amenity.py
@@ -0,0 +1,44 @@
+class Amenity:
+
+    def _init_(self, amenity_name, amenity_type, amenity_description, amenity_rates, amenity_status):
+        self.amenity_name = amenity_name
+        self.amenity_type = amenity_type
+        self.amenity_description = amenity_description
+        self.amenity_rates = amenity_rates
+        self.amenitiy_status = amenity_status
+    
+    #getter and setter for amenity name
+    def _set_amenity_name(self, input)
+        self.amenity_name = input
+    
+    def _get_amenity_name(self)
+        return self.amenity_name
+    
+    #getter and setter for amenity type
+    def _set_amenity_type(self, input)
+        self.amenity_type = input
+    
+    def _get_amenity_type(self)
+        return self.amenity_type
+
+    
+    #getter and setter for amenity description
+    def _set_amenity_description(self, input)
+        self.amenity_description = input
+    
+    def _get_amenity_description(self)
+        return self.amenity_description
+    
+    #getter and setter for amenity rates
+    def _set_amenity_rates(self, rates)
+        self.amenity_rates = rates
+    
+    def _get_amenity_rates(self)
+        return self.amenity_rates
+    
+    #getter and setter for amenity status
+    def _set_amenity_status(self, input)
+        self.amenity_status = input
+    
+    def _get_amenity_status(self)
+        return self.amenity_status
