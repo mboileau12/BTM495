f992af167fee7dd9f858f00ee44513d8b0006c7a
mboileau12 <matboileau@live.ca>
2023-02-12 19:08:10 -0500

    Classes

---
+++ HotelMortagneReservationSystem/CheckOut.py
@@ -0,0 +1,10 @@
+class CheckOut:
+
+    def _init_(self, check_out_time):
+        self.check_out_time = check_out_time
+
+    def _set_check_out_time(self, time)
+        self.check_out_time = check_out_time
+    
+    def _get_check_out_time(self)
+        return self.check_out_time
---
+++ HotelMortagneReservationSystem/DamageReport.py
@@ -0,0 +1,38 @@
+class DamageReport:
+
+    def _inti_(self, damage_id, damage_description, damage_date, damage_balance):
+        self.damage_id = damage_id
+        self.damage_description = damage_description
+        self.damage_date = damage_date
+        self.damage_balance = damage_balance
+
+    #getter and setter for damage id
+    def _set_damage_id(self, id)
+        self.damage_id = id
+    
+    def _get_damage_id(self)
+        return self.damage_id
+    
+    #getter and setter for damage description
+    def _set_damage_description(self, desc)
+        self.damage_description = desc
+    
+    def _get_damage_description(self)
+        return self.damage_description
+    
+    #getter and setter for damage date
+    def _set_damage_date(self, date)
+        self.damage_date = date
+    
+    def _get_damage_date(self)
+        return self.damage_date
+    
+
+    #getter and setter for damage balance
+    def _set_damage_balance(self, bal)
+        self.damage_balance = bal
+
+    def _get_damage_balance(self)
+        return self.damage_balance
+        
+
